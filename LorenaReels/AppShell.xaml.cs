13c914c272a89fde7b2cdafa533358be645e1046
Muhammad Yaseen <casemng@outlook.com>
2026-06-29 14:31:54 +0300

    Add project files.

---
+++ LorenaReels/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LorenaReels
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LorenaReels/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LorenaReels
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
